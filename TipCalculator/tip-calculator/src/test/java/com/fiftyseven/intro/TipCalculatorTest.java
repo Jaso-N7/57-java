2d450bf5320a792e2562276921cef3c9e5b42984
Jason Robinson <jasonsrobinson@protonmail.com>
2023-10-30 23:33:11 -0500

    Added new test scenarios to current tests. Failing tests

--- TipCalculator/tip-calculator/src/test/java/com/fiftyseven/intro/TipCalculatorTest.java
+++ TipCalculator/tip-calculator/src/test/java/com/fiftyseven/intro/TipCalculatorTest.java
@@ -11,13 +11,15 @@ import org.junit.jupiter.api.Test;
  */
 public class TipCalculatorTest 
 {
-    private TipCalculator tc;
-    private final static BigDecimal BILL_AMOUNT = BigDecimal.valueOf(10L);
-    private final static double TIP_RATE = 15.00D;
+    private TipCalculator tc_10, tc_11_25;
+    private final static BigDecimal BILL_AMOUNT_10 = new BigDecimal("10");
+    private final static BigDecimal BILL_AMOUNT_11_25 = new BigDecimal("11.25");
+    private final static double TIP_RATE_15 = 15.00D;
     
     @BeforeEach
     public void setup () {
-        tc = TipCalculator.instance(BILL_AMOUNT, TIP_RATE);
+        tc_10 = TipCalculator.instance(BILL_AMOUNT_10, TIP_RATE_15);
+        tc_11_25 = TipCalculator.instance(BILL_AMOUNT_11_25, TIP_RATE_15);
     }
     
     /**
@@ -26,29 +28,38 @@ public class TipCalculatorTest
     @Test
     public void tip_MustBeGreaterThanZero()
     {
-        assertTrue(tc.tip().signum() > 0);
+        assertTrue(tc_10.tip().signum() > 0);
+        assertTrue(tc_11_25.tip().signum() > 0);
     }
     
     @Test
     public void tip_CalculatedAtFifteenPercent() {
         
-        var tipAt15 = new BigDecimal("1.50");
+        var expectedTip10 = new BigDecimal("1.50");
+        var expectedTip1125 = new BigDecimal("1.69");
         
-        assertTrue(tc.tip().equals(tipAt15));
+        assertTrue(tc_10.tip().equals(expectedTip10));
+        assertTrue(tc_11_25.tip().equals(expectedTip1125));
     }
     
     @Test
     public void total_MustBeGreaterThanZero() {
-        assertTrue(tc.total().signum() > 0);
+        assertTrue(tc_10.total().signum() > 0);
+        assertTrue(tc_11_25.total().signum() > 0);
     }
     
     @Test
     public void total_WithFifteenPercentTipRate () {
         
-        var totalWith15 = new BigDecimal("11.50");
+        var expectedTotal10 = new BigDecimal("11.50");
+        var expectedTotal1125= new BigDecimal("12.94");
         
-        assertTrue(tc.total().equals(totalWith15));
+        assertTrue(tc_10.total().equals(expectedTotal10));
+        assertTrue(tc_11_25.total().equals(expectedTotal1125));
     }
     
-    
+//    private BigDecimal tipOracle (BigDecimal bill, double rate) {
+//        var percent = BigDecimal.valueOf(100.00D);
+//        return bill.multiply(BigDecimal.valueOf(rate).divide(percent));
+//    }
 }

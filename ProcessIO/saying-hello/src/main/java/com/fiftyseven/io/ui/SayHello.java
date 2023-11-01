f39b208b74db8c426f2d12eaa2cc269c86b2dbcd
Jason Robinson <jasonsrobinson@protonmail.com>
2023-10-31 23:37:16 -0500

    Exercise 1 completed with constraints. No challenges undertaken. All tests passed

--- ProcessIO/saying-hello/src/main/java/com/fiftyseven/io/ui/SayHello.java
+++ ProcessIO/saying-hello/src/main/java/com/fiftyseven/io/ui/SayHello.java
@@ -1,13 +1,28 @@
 package com.fiftyseven.io.ui;
 
+import com.fiftyseven.io.model.SayingHello;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Hello User!
  *
  */
 public class SayHello 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner s = new Scanner(System.in);
+        SayingHello hello;
+        
+        System.out.print( "What is your name? " );
+        String user = s.nextLine();
+        
+        if (user.isEmpty()) {
+            hello = SayingHello.at();
+        } else {
+            hello = SayingHello.at( user );
+        }
+        
+        System.out.println( hello.greeting() );
     }
 }

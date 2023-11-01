79549cdb0e2b9a23642006359f8631da11d98bd8
Jason Robinson <jasonsrobinson@protonmail.com>
2023-10-31 23:50:54 -0500

    Challenge: Write a new version of the program without using any variables. All tests passing

--- ProcessIO/saying-hello/src/main/java/com/fiftyseven/io/ui/SayHello.java
+++ ProcessIO/saying-hello/src/main/java/com/fiftyseven/io/ui/SayHello.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
  * Prompts for your name and prints a greeting using your name
  * 
  * @author Jason Robinson
- * @version 1.0, 31/10/23
+ * @version 1.1, 31/10/23
  */
 public class SayHello 
 {
@@ -18,18 +18,9 @@ public class SayHello
     public static void main( String[] args )
     {
         Scanner s = new Scanner(System.in);
-        SayingHello hello;
         
-        // TODO: 1. Write a new version of the program without using any variables.
         System.out.print( "What is your name? " );
-        String user = s.nextLine();
         
-        if (user.isEmpty()) {
-            hello = SayingHello.at();
-        } else {
-            hello = SayingHello.at( user );
-        }
-        
-        System.out.println( hello.greeting() );
+        System.out.println( SayingHello.at( s.nextLine() ).greeting() );
     }
 }
